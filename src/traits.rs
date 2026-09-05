6bc97557b543ad082158d614e7b1015ba46b4b13
Lukas <lukas.roesel@posteo.net>
2026-09-05 22:36:51 +0200

    Add structs and enums for effects and effect-parameters. Add some rudimentary functions to effect-traits.

--- src/traits.rs
+++ src/traits.rs
@@ -1,11 +1,23 @@
 #![allow(dead_code)]
 
+use crate::types::EffectParameter;
+
 pub trait RenderContext {}
 
-pub trait Effect {}
-pub trait GpuEffect {}
+pub trait Effect {
+    fn handle_parameter_update();
+    fn parameter_info();
+    fn render();
+}
+
+pub trait GpuEffect: Effect {
+    fn into_shader();
+    fn render_gpu();
+}
 
 // Change effect's state by handling parameter changes.
-pub trait ChangeEffect {}
+pub trait ChangeEffect {
+    fn handle_parameter(param: &EffectParameter);
+}
 
 pub trait GuiInterface {}
--- src/types.rs
+++ src/types.rs
@@ -5,14 +5,66 @@ type ScreenCoordinate = u16;
 type ColorIntensity = u8;
 type Color = [ColorIntensity; 4];
 
+type AvsInt = u32;
+type AvsFloat = f32;
+
 pub struct RenderContext {
     width: ScreenCoordinate,
     height: ScreenCoordinate,
     framebuffer: Vec<Vec<Color>>,
 }
 
-// ControlParameters
-pub enum EffectParameter {
-    Switch(bool),
-    ByteSlider(u8),
+// Effect structure
+pub struct Effect {
+    inner: EffectInner,
+    name: String,
+    description: String,
+    is_creatable: bool,
+}
+
+pub enum EffectInner {
+    Single(SingleEffect),
+    WithChildrenEffects(MetaEffect),
+}
+
+pub struct SingleEffect {
+    effect_type: EffectType,
+    parameters: Vec<EffectParameter>,
+}
+
+pub struct MetaEffect {
+    parametes: Vec<EffectParameter>,
+    children: Vec<Effect>,
+}
+
+pub enum EffectType {
+    Render,
+    Trans,
+    Misc,
+}
+
+pub struct EffectParameter {
+    is_global: bool,
+    parameter: ParameterComponent,
+    label: String,
+}
+
+// not yet complete
+pub enum ParameterComponent {
+    ParamBool(bool),
+    ParamInt {
+        value: AvsInt,
+        min: AvsInt,
+        max: AvsInt,
+    },
+    ParamFloat {
+        value: AvsFloat,
+        min: AvsFloat,
+        max: AvsFloat,
+    },
+    ParamString(String),
+    ParamSelect {
+        selectables: Vec<String>,
+    },
+    ParameterComposit(Vec<ParameterComponent>),
 }
